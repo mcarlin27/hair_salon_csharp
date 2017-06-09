4600809d91242b1bdd4f32be040b53c55b37049a
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-09 10:21:17 -0700

    initialize Stylist object; update README.md to reflect accurate table creation.

---
+++ Objects/Stylist.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _name;
+    private string _bio;
+
+    public Stylist(string Name, string Bio, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _bio = Bio;
+    }
+    public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool idEquality = this.GetId() == newStylist.GetId();
+        bool nameEquality = this.GetName() == newStylist.GetName();
+        bool bioEquality = this.GetBio() == newStylist.GetBio();
+        return (idEquality && nameEquality ** bioEquality);
+      }
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public string GetBio()
+    {
+      return _bio;
+    }
+  }
+}
