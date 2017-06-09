9a3da7f67911e684ee43bd5b7a8cd4b2b47170c8
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-09 10:44:53 -0700

    add Database.cs to project; add GetAll() and DeleteAll() methods to Stylist constructor; run and pass first StylistTest.

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -43,5 +43,43 @@ namespace HairSalon
     {
       return _bio;
     }
+
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        string stylistBio = rdr.GetString(2);
+        Stylist newStylist = new Stylist(stylistName, stylistBio, stylistId);
+        allStylists.Add(newStylist);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allStylists;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
   }
 }
