12e013bfc5358036166b38ddb8c9bd4b8c9c1ed1
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-09 14:58:53 -0700

    add CSS styling (mostly Bootstrap) to Views; initialize pages to view all Salons/Stylists/Clients.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,21 @@ namespace HairSalon
       Get["/"] = _ => {
         return View["index.cshtml"];
       }; //homepage
+
+      Get["/salons"] = _ => {
+        List<Salon> allSalons = Salon.GetAll();
+        return View["salons.cshtml", allSalons];
+      }; //list of all salons
+
+      Get["/stylists"] = _ => {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["stylists.cshtml", allStylists];
+      }; //list of all stylists
+
+      Get["/clients"] = _ => {
+        List<Client> allClients = Client.GetAll();
+        return View["clients.cshtml", allClients];
+      }; //list of all clients
     }
   }
 }
